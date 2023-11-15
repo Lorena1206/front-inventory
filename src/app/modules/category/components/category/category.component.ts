14919d001ef50c31495aaa086daded63b0fcb3f1
lorenaH <hlorena27@gmail.com>
2023-11-15 10:01:48 -0500

    arreglo paginador

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
 import { NewCategoryComponent } from '../new-category/new-category.component';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 import { ConfirmComponent } from 'src/app/modules/shared/comoponents/confirm/confirm.component';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-category',
@@ -24,6 +25,9 @@ export class CategoryComponent implements OnInit {
 
   displayedColumns: string[] = ['id','name','description','actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
 
   getCategories(): void{
@@ -49,6 +53,7 @@ export class CategoryComponent implements OnInit {
       });
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+      this.dataSource.paginator = this.paginator;
     }
   }
 
