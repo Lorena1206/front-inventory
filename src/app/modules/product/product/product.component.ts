cc88e5ec115aa49ccc107bd18afb0112ea3ee714
lorenaH <hlorena27@gmail.com>
2023-11-28 18:15:03 -0500

    guardar productos

--- src/app/modules/product/product/product.component.ts
+++ src/app/modules/product/product/product.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild, inject } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductService } from '../../shared/services/product.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
+import { NewProductComponent } from '../new-product/new-product.component';
 
 @Component({
   selector: 'app-product',
@@ -11,6 +14,8 @@ import { ProductService } from '../../shared/services/product.service';
 export class ProductComponent {
 
   private productService = inject(ProductService);
+  private dialog = inject( MatDialog);
+  private snackbar = inject(MatSnackBar);
 
   ngOnInit(): void {
     this.getProducts();
@@ -52,6 +57,30 @@ export class ProductComponent {
       this.dataSource = new MatTableDataSource<ProductElement>(dateProduct);
       this.dataSource.paginator = this.paginator;
     }
+  }
+
+
+  openProductDialog(){
+    const dialogRef = this.dialog.open(NewProductComponent, {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if( result ==1){
+        this.openSnackBar("Producto agregado", "Exitoso");
+        this.getProducts();
+      } else if (result ==2){
+        this.openSnackBar("Se produjo un error al guardar producto", "Error")
+      }
+     
+    });
+  }
+
+
+  openSnackBar(message: string, action:string) : MatSnackBarRef<SimpleSnackBar>{
+    return this.snackbar.open(message, action, {
+      duration: 2000
+    })
   }
 
 }
--- src/app/modules/shared/services/product.service.ts
+++ src/app/modules/shared/services/product.service.ts
@@ -20,4 +20,10 @@ export class ProductService {
     const endpoint = `${base_url}/products`;
     return this.http.get(endpoint);
   }
+
+  //*** save the product*/
+   saveProduct(body:any){
+    const endpoint = `${base_url}/products`;
+    return this.http.post(endpoint, body)
+   }
 }
